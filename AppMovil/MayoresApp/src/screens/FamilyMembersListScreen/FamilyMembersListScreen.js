a2116f38177858666f885dca1d6151a75ea6ac1c
johanaromina <112700663+johanaromina@users.noreply.github.com>
2024-02-21 18:32:27 -0300

    alarmas funcionando

--- AppMovil/MayoresApp/src/screens/FamilyMembersListScreen/FamilyMembersListScreen.js
+++ AppMovil/MayoresApp/src/screens/FamilyMembersListScreen/FamilyMembersListScreen.js
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, Modal, Image, StyleSheet, useWindowDimensions, Linking } from 'react-native';
-import { sendWhatsapp } from 'react-native-send-intent'; // Importa sendWhatsapp
+import { View, Text, Modal, Image, StyleSheet, useWindowDimensions, Linking, TouchableOpacity } from 'react-native';
 import CustomButton from '../../components/CustomButton/CustomButton';
 import CustomInput from '../../components/CustomInput/CustomInput';
 import { useNavigation } from '@react-navigation/native';
-
-import logo from '../../../assets/foto1.png';
 
 const FamilyMembersListScreen = () => {
   const username = '...';
@@ -34,13 +31,37 @@ const FamilyMembersListScreen = () => {
   };
 
   const sendAlarm = () => {
-    // Implementar la lógica para enviar una alarma por WhatsApp al familiar seleccionado.
+    let message = '';
+
+    switch (alarmType) {
+      case 'verde':
+        message = 'Me encuentro bien';
+        break;
+      case 'amarilla':
+        message = 'Hoy no es un buen día';
+        break;
+      case 'roja':
+        message = 'Por favor ven a verme, no me siento bien';
+        break;
+      default:
+        message = '';
+    }
+
+    const phoneNumber = '2604618942';
+    const url = `whatsapp://send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+
+    Linking.openURL(url)
+      .then(() => {
+        console.log('Mensaje enviado exitosamente');
+      })
+      .catch((error) => {
+        console.error('Error al enviar el mensaje:', error);
+      });
+
     closeAlarmModal();
   };
 
   const handleSendWhatsapp = () => {
-    // Reemplaza '+5492604618942' con el número de teléfono al que deseas enviar el mensaje
-    // y 'Mensaje de prueba' con el mensaje que deseas enviar.
     const phoneNumber = '+5492604618942';
     const message = 'Mensaje de prueba';
     const url = `whatsapp://send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
@@ -56,7 +77,7 @@ const FamilyMembersListScreen = () => {
     <View style={styles.container}>
       <Text style={styles.welcomeText}>Bienvenida/o {username}</Text>
       <Image
-        source={logo}
+        source={require('../../../assets/foto1.png')}
         style={[styles.logo, { height: height * 0.3, width: width * 0.7 }]}
         resizeMode="contain"
       />
@@ -72,11 +93,15 @@ const FamilyMembersListScreen = () => {
 
       <Text style={styles.alarmTitle}>Alarmas</Text>
       <View style={styles.alarmContainer}>
-        <View style={styles.semaphore}>
+        <TouchableOpacity onPress={() => openAlarmModal('roja')}>
           <View style={[styles.alarmButton, { backgroundColor: '#D50000' }]} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => openAlarmModal('amarilla')}>
           <View style={[styles.alarmButton, { backgroundColor: '#FFD600' }]} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => openAlarmModal('verde')}>
           <View style={[styles.alarmButton, { backgroundColor: '#388E3C' }]} />
-        </View>
+        </TouchableOpacity>
       </View>
       
       <Text style={styles.emergencyText}>Si tienes una emergencia, llama a {username}</Text>
@@ -178,10 +203,6 @@ const styles = StyleSheet.create({
     height: 30,
     borderRadius: 15,
     marginHorizontal: 5,
-  },
-  semaphore: {
-    flexDirection: 'row',
-    alignItems: 'center',
   },
 });
 
